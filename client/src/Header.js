8c6eb8187d69cc3eceff074784d9d708e1211ee7
Zhihan Hu <89308955+h-zhh@users.noreply.github.com>
2023-03-19 22:49:25 +0100

    Add Header.js

---
+++ client/src/Header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+  <header>
+    <a href="" className="logo">
+      TechBlog
+    </a>
+    <nav>
+      <a href="">Login</a>
+      <a href="">Register</a>
+    </nav>
+  </header>;
+}
