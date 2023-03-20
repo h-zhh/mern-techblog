62d82ac32afa5f9c7ae2e3dc34bbed4b116d3565
Zhihan Hu <89308955+h-zhh@users.noreply.github.com>
2023-03-20 20:00:57 +0100

    Add register POST request and mongoose

--- api/index.js
+++ api/index.js
@@ -3,9 +3,13 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.post("/register", (req, res) => {
-  res.json("test ok2");
+  const { username, password } = req.body;
+  res.json({ requestData: { username, password } });
 });
 
 app.listen(4000);
+
+//mongodb+srv://blog:idEV9wjqm4zwqFTF@cluster0.elgjhvw.mongodb.net/?retryWrites=true&w=majority
