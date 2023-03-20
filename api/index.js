04e3efa2f75d97794bda3e97a13c3f3b26d16746
Zhihan Hu <89308955+h-zhh@users.noreply.github.com>
2023-03-20 20:18:48 +0100

    Add encryption of password and connect mongoose

--- api/index.js
+++ api/index.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+const User = require("./models/User");
+const bcrypt = require("bcryptjs");
 const app = express();
+
+const saltRounds = 10;
+const salt = bcrypt.genSaltSync(saltRounds);
 
 app.use(cors());
 app.use(express.json());
 
-app.post("/register", (req, res) => {
+mongoose.connect(
+  "mongodb+srv://blog:idEV9wjqm4zwqFTF@cluster0.elgjhvw.mongodb.net/?retryWrites=true&w=majority"
+);
+
+app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  res.json({ requestData: { username, password } });
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(400).json(e);
+  }
 });
 
 app.listen(4000);
-
-//mongodb+srv://blog:idEV9wjqm4zwqFTF@cluster0.elgjhvw.mongodb.net/?retryWrites=true&w=majority
