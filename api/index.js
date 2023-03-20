e659d9f942b39ed44aa47ed172683d7a53ccb052
Zhihan Hu <89308955+h-zhh@users.noreply.github.com>
2023-03-20 20:40:20 +0100

    Add login POST request to index.js

--- api/index.js
+++ api/index.js
@@ -4,11 +4,13 @@ const { default: mongoose } = require("mongoose");
 const User = require("./models/User");
 const bcrypt = require("bcryptjs");
 const app = express();
+const jwt = require("jsonwebtoken");
 
 const saltRounds = 10;
 const salt = bcrypt.genSaltSync(saltRounds);
+const secret = "djkaleaoreiaoi32o4u";
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 
 mongoose.connect(
@@ -25,6 +27,21 @@ app.post("/register", async (req, res) => {
     res.json(userDoc);
   } catch (e) {
     res.status(400).json(e);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    // logged in
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json("ok");
+    });
+  } else {
+    res.status(400).json("wrong credentials");
   }
 });
 
